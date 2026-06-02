060940af7405f2261f17cf1bbdb6f1dc486adec4
Martin <martin.wege@service-health.de>
2026-06-02 23:14:40 +0200

    UHP-28 Make subscriptions Thread safe

--- server/src/main/java/de/gematik/ws/conn/eventservice/wsdl/v7_2/Subscriptions.java
+++ server/src/main/java/de/gematik/ws/conn/eventservice/wsdl/v7_2/Subscriptions.java
@@ -1,25 +1,44 @@
 package de.gematik.ws.conn.eventservice.wsdl.v7_2;
 
 import de.gematik.ws.conn.eventservice.v7.SubscriptionType;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Predicate;
 
 @ApplicationScoped
 public class Subscriptions {
 
+  @Inject private MeterRegistry registry;
+
+  @PostConstruct
+  private void init() {
+
+    Gauge.builder("jvm.threads.peak", this, Subscriptions::subscriptionCount)
+        .description("The peak live thread count...") // optional
+        .tags("key", "value") // optional
+        .register(registry);
+  }
+
   private final Map<String, List<SubscriptionType>> tlsCertCN2subscriptions =
       new ConcurrentHashMap<>();
 
-  public synchronized void addSubscription(String tlsCertCn, SubscriptionType type) {
-    var subscriptionsList =
-        tlsCertCN2subscriptions.computeIfAbsent(tlsCertCn, (key) -> new CopyOnWriteArrayList<>());
-    subscriptionsList.add(type);
+  public void addSubscription(String tlsCertCn, SubscriptionType type) {
+    tlsCertCN2subscriptions.computeIfAbsent(tlsCertCn, (key) -> new ArrayList<>());
+    tlsCertCN2subscriptions.computeIfPresent(
+        tlsCertCn,
+        (key, val) -> {
+          val.add(type);
+          return val;
+        });
   }
 
   public List<SubscriptionType> getSubscriptions(String tlsCertCn) {
